6dde8d1c886eeb0a120d2726fb5cb11666a48fca
Krystian Hantulik <xkrystox@outlook.com>
2021-11-18 19:35:44 +0100

    Dodaj pliki projektów.

---
+++ Koszyk/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Koszyk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.cenaLbl = new System.Windows.Forms.Label();
+            this.cenaTotalLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(423, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(320, 342);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(12, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 100);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ORZECH";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(146, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 100);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "JABŁKO";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(279, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 100);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "WODA";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(279, 147);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 100);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "BUŁKA";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(146, 147);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 100);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "MONSTER";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button6.Location = new System.Drawing.Point(12, 147);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(118, 100);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "LÓD EKIPY";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button7.Location = new System.Drawing.Point(279, 262);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(118, 100);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "KONDOMY";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button8.Location = new System.Drawing.Point(146, 262);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(118, 100);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "CHIPSY";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button9.Location = new System.Drawing.Point(12, 262);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(118, 100);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "PIWO";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Georgia", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown1.Location = new System.Drawing.Point(146, 377);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(118, 47);
+            this.numericUpDown1.TabIndex = 10;
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.button10.Location = new System.Drawing.Point(279, 377);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(118, 47);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "DODAJ";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.button11.Location = new System.Drawing.Point(625, 377);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(118, 47);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "KUP";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // cenaLbl
+            // 
+            this.cenaLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.cenaLbl.Location = new System.Drawing.Point(12, 377);
+            this.cenaLbl.Name = "cenaLbl";
+            this.cenaLbl.Size = new System.Drawing.Size(116, 47);
+            this.cenaLbl.TabIndex = 13;
+            this.cenaLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cenaTotalLbl
+            // 
+            this.cenaTotalLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.cenaTotalLbl.Location = new System.Drawing.Point(461, 380);
+            this.cenaTotalLbl.Name = "cenaTotalLbl";
+            this.cenaTotalLbl.Size = new System.Drawing.Size(116, 47);
+            this.cenaTotalLbl.TabIndex = 14;
+            this.cenaTotalLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cenaTotalLbl);
+            this.Controls.Add(this.cenaLbl);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label cenaLbl;
+        private System.Windows.Forms.Label cenaTotalLbl;
+    }
+}
+
